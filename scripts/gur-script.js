c93e398c92f2ae3d19d9aeadd0b4b419814c11dc
Adam King <adk486@gmail.com>
2016-09-20 10:10:46 -0700

    add pm2 scripts

---
+++ scripts/gur-script.js
@@ -0,0 +1,4 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("/root/go-ur/build/bin/gur --bootnodes enode://fcf730cf678d6296ffa75a1b2c06aa07d9558788762d0bbefbc209ccbfb4e840f7dcfc2f7a188eb2e65056d989de3722df3fc4df286eb3690d4586992c1c6d82@138.68.63.204:19595,enode://d846b3c0445b7a91cfeb56fbeaece55ca9e559a6e5810cc41c54e2b88790fa7a24444508f16eb983630da1367ab73a6db1b705cc36134d9e61a2df070284d3f4@138.68.56.175:19595 --networkid 15 --datadir ~/.ur", puts);
