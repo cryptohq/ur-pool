b673ca906b1fe64c12b284c31e7aa5cdcda3da64
Adam King <adk486@gmail.com>
2016-09-20 10:17:30 -0700

    script updates

--- scripts/frontend-script.js
+++ scripts/frontend-script.js
@@ -1,4 +1,4 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("../www/build.sh", puts);
+exec("/root/ur-pool/www/build.sh", puts);
